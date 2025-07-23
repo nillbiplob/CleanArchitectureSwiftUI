15e280f3da7e8bd3b4970c612d82b911b14217ef
Md Shafiul Alam Biplob <javaorjava@gmail.com>
2025-07-23 12:24:28 +0600

    Initial Commit

---
+++ CleanArchitectureSwiftUI/CleanArchitectureSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureSwiftUIApp.swift
+//  CleanArchitectureSwiftUI
+//
+//  Created by Shafiul Alam Biplob on 23/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanArchitectureSwiftUITests/CleanArchitectureSwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  CleanArchitectureSwiftUITests.swift
+//  CleanArchitectureSwiftUITests
+//
+//  Created by Shafiul Alam Biplob on 23/7/25.
+//
+
+import Testing
+
+struct CleanArchitectureSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
